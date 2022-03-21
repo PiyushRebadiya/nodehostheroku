8ab684efc32b58e0112177c81254e9cc6c3779d8
PiyushRebadiya <piyushrebadiya123@gmail.com>
2022-03-21 15:04:11 +0530

    some change in main

--- Model/banner.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-    photo: String,
-})
-
-
-module.exports = mongoose.model("banner", UserSchema)
--- controller/mobile_number.js
+++ controller/mobile_number.js
@@ -9,17 +9,6 @@ const mobile_user_create = async (req, res) => {
         let mobile_number_code = req.body.mobile_number;
     
         let code = "123456"
-        var options = { authorization: "5YyVe6SvLbtNioBc10nmh2gPEDRxf4aJ9HMQAIwsUd3ZqXOGz8Z3RAXpaOT2qlsjI7Y6DFEWLw4uQxJH", message: `This is a test OTP Code Message Your OPT Code is ${code}`, numbers: [mobile_number_code] }
-       let data = fast2sms.sendMessage(options).then((response) => {
-            console.log("response++++++++++++",response);
-            return response
-        }).then((reqest) => {
-            console.log("++++++++++reqest",reqest);
-
-        })
-
-
-        console.log("data",data);
       
         try {
           const token = jwt.sign(
