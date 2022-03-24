c258877a92d291ace2aafeb3ec53ada2b1d6eed1
subham-o-trustclarity <subham.o@trustclarity.com>
2022-03-24 11:51:38 +0530

    register router change

--- Route/user.js
+++ Route/user.js
@@ -6,13 +6,14 @@ const home_page_Controller = require("../controller/home_page")
 const home_greeting_Controller = require("../controller/home_greeting")
 const auth = require("../middleware/auth")
 
-router.post("/register", userController.user_create);
-router.post("/connect", mobileController.mobile_user_create);
-router.get("/", userController.user_all); 
+// router.post("/register", userController.user_create);
+router.post("/register", mobileController.mobile_user_create);
+router.get("/register", mobileController.mobile_user_all);
+// router.get("/", userController.user_all); 
 router.get("/data",auth, userController.showData);
 router.get("/banner",auth, Controller_banner.banner_slider_fun);
 router.post("/banner",auth, Controller_banner.banner_slider_create);
-router.post("/login", userController.login_user_fun);
+// router.post("/login", userController.login_user_fun);
 router.get("/homedata",auth, home_page_Controller.home_page_data_show);
 router.get("/greeting",auth, home_greeting_Controller.home_greeting_show);
 router.post("/homedata",auth, home_page_Controller.home_page_data_create);
@@ -21,8 +22,8 @@ router.post("/homedata/:id",auth, home_page_Controller.home_image_create);
 router.post("/greeting/:id",auth, home_greeting_Controller.home_greeting_image_create);
 router.get("/homedata/image",auth, home_page_Controller.home_image_data_show);
 router.get("/greeting/image",auth, home_greeting_Controller.home_greeting_image_data_show);
-router.get("/register/:productId", userController.user_details);
-router.put("/register/:productId", userController.user_update);
-router.delete("/register/:productId", userController.user_delete);
+// router.get("/register/:productId", userController.user_details);
+// router.put("/register/:productId", userController.user_update);
+// router.delete("/register/:productId", userController.user_delete);
 
 module.exports = router;
--- controller/mobile_number.js
+++ controller/mobile_number.js
@@ -4,40 +4,93 @@ const fast2sms = require('fast-two-sms')
 
 const mobile_user_create = async (req, res) => {
 
-    
-    if(/^\d{10}$/.test(req.body.mobile_number)) {
-        let mobile_number_code = req.body.mobile_number;
-    
-        let code = "123456"
-      
-        try {
-          const token = jwt.sign(
-            {mobile_number: mobile_number_code },
-            process.env.SECRET_KEY,
-            {
-              expiresIn: "2h",
-            }
-          );
-      
-          res.cookie(`jwToken`, token);
-      
-          // user
-          res.status(200).json({
-              "status":true,
-              "data": [{mobile_number:mobile_number_code,token:token}],
-              "code": code
-          }); 
-      
-        } catch (error) {
-          console.log("error", error);
-      
-        }
 
+  if (/^\d{10}$/.test(req.body.mobile_number)) {
+    const usersMobileData = await Mobile.find({ mobile_number: req.body.mobile_number })
+
+    if (usersMobileData && usersMobileData.length == 0) {
+
+      let mobile_number_code = await new Mobile({
+        mobile_number: req.body.mobile_number,
+        password: 123456
+      });
+
+      try {
+        await mobile_number_code.save()
+        const token = jwt.sign(
+          mobile_number_code.toJSON(),
+          process.env.SECRET_KEY,
+          {
+            expiresIn: "1h",
+          }
+        );
+
+        res.cookie(`jwToken`, token);
+
+        // user
+        res.status(200).json({
+          "status": true,
+          "data": [{
+            mobile_number: mobile_number_code.mobile_number,
+            password: 123456,
+            token: token
+          }]
+        });
+
+      } catch (error) {
+        console.log("error", error);
+
+      }
+    } else {
+      let mobile_number_code = {
+        mobile_number: req.body.mobile_number,
+        password: 123456
+      };
+
+      try {
+        const token = jwt.sign(
+          mobile_number_code,
+          process.env.SECRET_KEY,
+          {
+            expiresIn: "1h",
+          }
+        );
+
+        res.cookie(`jwToken`, token);
+
+        // user
+        res.status(200).json({
+          "status": true,
+          "data": [{
+            mobile_number: mobile_number_code.mobile_number,
+            password: 123456,
+            token: token
+          }]
+        });
+
+      } catch (error) {
+        console.log("error", error);
+
+      }
     }
 
-  
+
+  }
+
+
 }
 
+
+const mobile_user_all = async (req, res) => {
+  try {
+    const mobile_user = await Mobile.find()
+    res.clearCookie(`jwToken`);
+    res.json(mobile_user)
+  } catch (error) {
+    res.json({ message: error });
+  }
+}
 module.exports = {
-    mobile_user_create
+  mobile_user_create,
+  mobile_user_all
 }
