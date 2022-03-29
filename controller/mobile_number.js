7ceb42bc12a619dc96ea899d247511e331fa007e
PiyushRebadiya <piyushrebadiya123@gmail.com>
2022-03-29 10:05:56 +0530

    change register api data

--- controller/mobile_number.js
+++ controller/mobile_number.js
@@ -6,13 +6,28 @@ const mobile_user_create = async (req, res) => {
 
 
   if (/^\d{10}$/.test(req.body.mobile_number)) {
-    const usersMobileData = await Mobile.find({ mobile_number: req.body.mobile_number })
 
+    if (req.body.password && req.body.mobile_number) {
+     return verify_password(req,res)
+    }
+
+    return res.status(201).send({ message: "Successfully" })
+
+  } else {
+    return res.status(404).send({ message: "Please Enter 10 digit mobile number" })
+  }
+
+
+}
+
+async function verify_password (req,res){
+  if (/^\d{6}$/.test(req.body.password)) {
+    const usersMobileData = await Mobile.find({ mobile_number: req.body.mobile_number })
     if (usersMobileData && usersMobileData.length == 0) {
 
       let mobile_number_code = await new Mobile({
         mobile_number: req.body.mobile_number,
-        password: 123456
+        password: req.body.password
       });
 
       try {
@@ -34,56 +49,59 @@ const mobile_user_create = async (req, res) => {
             displayImage: mobile_number_code.displayImage,
             logoImage: mobile_number_code.displayImage,
             mobile_number: mobile_number_code.mobile_number,
-            password: mobile_number_code.password,
             userId: mobile_number_code._id,
             token: token
           }]
         });
 
       } catch (error) {
-        console.log("error", error);
+        // console.log("error", error);
+        res.error({ message: "please check your password..." })
 
       }
     } else {
-      let mobile_number_code = {
-        mobile_number: req.body.mobile_number,
-        password: 123456
-      };
+      if (usersMobileData[0].password == req.body.password) {
+        let mobile_number_code = {
+          mobile_number: req.body.mobile_number,
+          password: req.body.password
+        };
 
-      try {
-        const token = jwt.sign(
-          mobile_number_code,
-          process.env.SECRET_KEY,
-          {
-            expiresIn: "1h",
-          }
-        );
+        try {
+          const token = jwt.sign(
+            mobile_number_code,
+            process.env.SECRET_KEY,
+            {
+              expiresIn: "1h",
+            }
+          );
 
-        res.cookie(`jwToken`, token);
+          res.cookie(`jwToken`, token);
 
-        // user
-        res.status(200).json({
-          "status": true,
-          "data": [{
-            displayImage: usersMobileData[0].displayImage,
-            logoImage: usersMobileData[0].logoImage,
-            mobile_number: usersMobileData[0].mobile_number,
-            password: usersMobileData[0].password,
-            userId: usersMobileData[0]._id,
-            token: token
-          }]
-        });
+          // user
+          res.status(200).json({
+            "status": true,
+            "data": [{
+              displayImage: usersMobileData[0].displayImage,
+              logoImage: usersMobileData[0].logoImage,
+              mobile_number: usersMobileData[0].mobile_number,
+              userId: usersMobileData[0]._id,
+              token: token
+            }]
+          });
 
-      } catch (error) {
-        console.log("error", error);
+        } catch (error) {
+          // console.log("error", error);
 
+          res.status(404).send({ message: error })
+        }
+      } else {
+        res.status(404).send({ message: "Please Enter Your True Password..." })
       }
+
     }
-
-
+  } else {
+    res.status(404).send({ message: "Please Enter 6 Digit Verify Number..." })
   }
-
-
 }
 
 const mobile_user_update = async (req, res) => {
@@ -94,16 +112,16 @@ const mobile_user_update = async (req, res) => {
     logoImage: req.body.logoImage,
   }
 
-    try {
+  try {
 
-      const updateUsers = await Mobile.findByIdAndUpdate(
-        { _id: req.params.id },
-        users
-      );
-      res.json(updateUsers);
-    } catch (error) {
-      res.json({ message: error });
-    }
+    const updateUsers = await Mobile.findByIdAndUpdate(
+      { _id: req.params.id },
+      users
+    );
+    res.json(updateUsers);
+  } catch (error) {
+    res.json({ message: error });
+  }
 
 }
 
